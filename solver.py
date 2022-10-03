e5e173581a2a2c9e26768b08315f463b86b2bb03
Mahmoud Hawara <66390077+MahmoudHawara@users.noreply.github.com>
2022-10-03 17:44:16 +0200

    Update solver.py

--- solver.py
+++ solver.py
@@ -1,3 +1,4 @@
+from distutils.command.build import build
 from college import Branch
 from groups_input import group
 from arabic_reshaper import reshape
@@ -5,9 +6,8 @@ from arabic_reshaper import reshape
 mem, OO = dict(), int(1e18)
 wrongForBuild, wrongForFloor, wrongForHall = int(1e6), int(1e3), 1
 toPrint = []
-uniqueGroups = set()
 
-def dp(idx, msk, g, h)->int:
+def dp(idx, msk, g, h, l)->int:
     if (idx == len(h)):
         if msk != ((1 << len(g)) - 1): return OO
         else: return 0
@@ -21,6 +21,9 @@ def dp(idx, msk, g, h)->int:
         sm, j, weight = h[idx].volume, idx + 1, 0
         while j < len(h) and sm < group[g[i]].volume:
             sm += h[j].volume
+            if j // l != (j - 1) // l:
+                weight = OO
+                break
             if h[j].buildNum != h[j - 1].buildNum:
                 weight += wrongForBuild
                 j += 1
@@ -31,28 +34,35 @@ def dp(idx, msk, g, h)->int:
                 continue
             weight += wrongForHall
             j += 1
-        if sm < group[g[i]].volume: continue
-        mem[(idx, msk)] = min(mem[(idx, msk)], weight + dp(j, msk | (1 << i), g, h), OO)
+        if sm < group[g[i]].volume or weight == OO: continue
+        mem[(idx, msk)] = min(mem[(idx, msk)], weight + dp(j, msk | (1 << i), g, h, l), OO)
     
-    mem[(idx, msk)] = min(mem[(idx, msk)], dp(idx + 1, msk, g, h), OO)
+    mem[(idx, msk)] = min(mem[(idx, msk)], dp(idx + 1, msk, g, h, l), OO)
     
     return mem[(idx, msk)]
 
 
-def buildDp(idx, msk, g, h)->None:
+def buildDp(idx, msk, g, h, l)->None:
     if idx == len(h):
-        for hall, gro, _from, to in toPrint:
-            print(f"{reshape(hall)[::-1]}    {reshape(gro)[::-1]}    {_from}    {to}")
+        for i in range(len(toPrint)):
+            if i % l == 0:
+                print('\n\n-----------------------------------------------------\n\n')
+                print(f"Day {i // l + 1}.")
+            hall, gro, _from, to = toPrint[i][0], toPrint[i][1], toPrint[i][2], toPrint[i][3]
+            print(f"{hall}  {reshape(gro)[::-1]} {_from}    {to}")
         print("\n\n==========================================================\n\n")
         return
     
-    optimal = dp(idx, msk, g, h)
+    optimal = dp(idx, msk, g, h, l)
 
     for i in range(len(g)):
         if msk & (1 << i): continue
         sm, j, weight = h[idx].volume, idx + 1, 0
         while j < len(h) and sm < group[g[i]].volume:
             sm += h[j].volume
+            if j // l != (j - 1) // l:
+                weight = OO
+                break
             if h[j].buildNum != h[j - 1].buildNum:
                 weight += wrongForBuild
                 j += 1
@@ -63,56 +73,77 @@ def buildDp(idx, msk, g, h)->None:
                 continue
             weight += wrongForHall
             j += 1
-        if sm < group[g[i]].volume: continue
-        if (weight + dp(j, msk | (1 << i), g, h) == optimal):
+        if sm < group[g[i]].volume or weight == OO: continue
+        if (weight + dp(j, msk | (1 << i), g, h, l) == optimal):
             sm, j, initial = 0, idx, group[g[i]]._from
             while j < len(h) and sm < group[g[i]].volume:
                 toPrint.append((h[j].name, group[g[i]].name, initial, min(initial + h[j].volume - 1, group[g[i]].to)))
                 sm += h[j].volume
                 initial += h[j].volume
                 j += 1
-            buildDp(j, msk | (1 << i), g, h)
+            buildDp(j, msk | (1 << i), g, h, l)
             return
     
     toPrint.append((h[idx].name, 'فارغه', '       ---', '---'))
-    buildDp(idx + 1, msk, g, h)
+    buildDp(idx + 1, msk, g, h, l)
     return
 
 def solve(branch :Branch)->None:
     
-    groups = branch.groupsInBranch
+    groups = branch.groupsInBranch.copy()
+    print(groups)
+    allHalls = branch.hallsInBranch.copy()
+    allHalls = allHalls + branch.hallsInBranch.copy()
+    allHalls = allHalls + branch.hallsInBranch.copy()  
+    
+    mem.clear()
 
-    for i in range(1 << len(groups)):  
-        iHave, g1, cnt = [], [], 0
-        for j in range(len(groups)):
-            iHave.append(j)
-        for j in range(len(groups)):
-            if i & (1 << j):
-                g1.append(iHave[j - cnt])
-                iHave.pop(j - cnt)
-                cnt += 1
+    if dp(0, 0, groups, allHalls, len(allHalls) // 3) == OO:
+        print("NO Valid Option")
+        return
+    
+    print("1. Optimal Solution")
+    print("2. Test")
+
+    choice = input("Enter Your Choice: ")
+    while choice.isnumeric() == False or int(choice) < 1 or int(choice) > 2:
+        choice = input("Enter valid number: ")
+    choice = int(choice)
+
+    if choice == 1:
+        buildDp(0, 0, groups, allHalls, len(allHalls) // 3)
+        return
+
+    mem.clear()
+    toPrint.clear()
+
+    choice = input("Enter The Number of Dayes which you need to build: ")
+    while choice.isnumeric() == False or int(choice) < 1 or int(choice) > 3:
+        choice = input("Enter valid number: ")
+    choice = int(choice)
+
+    if choice == 1:
+        g1, g2 = list(), list()
+
+        choice = input("Enter The Number of groups in that Day: ")
+        while choice.isnumeric() == False or int(choice) < 1 or int(choice) > len(groups):
+            choice = input("Enter valid number: ")
+        choice = int(choice)
+
+        for i in range(choice):
+            print('\n\n-------------------\n\n')
+            for j in range(len(groups)):
+                print(j + 1, '. ', reshape(group[groups[j]].name)[::-1])
+            print('\n\n-------------------\n\n')
+            g = input(f"Choose The Group: ")
+            while g.isnumeric() == False or int(g) < 1 or int(g) > len(groups):
+                g = input("Enter valid number: ")
+            g = int(g)
+            g1.append(groups[g - 1])
+            groups.pop(g - 1)
+
+        # for 
+
         
-        for ii in range(1 << len(iHave)):
-            g2, g3 = [], []
-            for j in range(len(iHave)):
-                if ii & (1 << j):
-                    g2.append(iHave[j])
-                else:
-                    g3.append(iHave[j])
-            
-            mem.clear()
-            if dp(0, 0, g1, branch.hallsInBranch) == OO: continue
-            mem.clear()
-            if dp(0, 0, g2, branch.hallsInBranch) == OO: continue
-            mem.clear()
-            if dp(0, 0, g3, branch.hallsInBranch) == OO: continue
-            # mem.clear()
-            # buildDp(0, 0, g1, branch.hallsInBranch)
-            # mem.clear()
-            # buildDp(0, 0, g2, branch.hallsInBranch)
-            # mem.clear()
-            # buildDp(0, 0, g3, branch.hallsInBranch)
-            uniqueGroups.add(tuple(sorted([tuple(g1), tuple(g2), tuple(g3)])))
 
-    # print(uniqueGroups)
 
